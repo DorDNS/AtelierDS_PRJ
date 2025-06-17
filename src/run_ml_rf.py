8e85bb72e72f6d65609f983959b25ef4047d52a2
Thomas Fischer <thomas.fischer67@outlook.fr>
2025-06-17 16:25:23 +0200

    Revert "séparation exécution ml"
    This reverts commit 5add2f581744a0748fa81bb8b92670593964a980.

--- src/run_ml_rf.py
+++
@@ -1,7 +0,0 @@
-from ml_supervised import RandomForest, load_data
-
-df = load_data("data/processed/final_dataset.csv", sample_size=10000)
-
-# Étape  pour estimer un bon k
-#evaluate_kmeans_k(df)
-RandomForest(df)
